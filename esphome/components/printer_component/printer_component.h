1d6780f69f931f22ade69296c502998859f665a1
Josh Graff <github@joshuagraff.com>
2023-11-08 21:13:57 -0500

    includes work

--- esphome/components/printer_component/printer_component.h
+++ esphome/components/printer_component/printer_component.h
@@ -3,7 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
 #include "test.h"
-#include "thermalprinter/thermalprinter.h"
+#include "thermalprinter.h"
 
 namespace esphome {
 namespace printer_component {
