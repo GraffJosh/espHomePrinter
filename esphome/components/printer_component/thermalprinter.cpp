62bae1df5c1c89982100114d23ac703a216c6662
Josh Graff <github@joshuagraff.com>
2023-11-10 21:15:22 -0500

    Not calling the right write?

--- esphome/components/printer_component/thermalprinter.cpp
+++ esphome/components/printer_component/thermalprinter.cpp
@@ -22,11 +22,11 @@ Epson::Epson()
 
 void Epson::dump_config(){
     ESP_LOGCONFIG(TAG, "JPGIndustries Printer component");
-    int result = getStatus();
-    char string_buffer[(sizeof(result)) + 1];
-    memcpy(string_buffer, &result, sizeof(result));
-    string_buffer[sizeof(result)] = 0; // Null termination.
-    ESP_LOGCONFIG(TAG, string_buffer);
+    // int result = getStatus();
+    // char string_buffer[(sizeof(result)) + 1];
+    // memcpy(string_buffer, &result, sizeof(result));
+    // string_buffer[sizeof(result)] = 0; // Null termination.
+    // ESP_LOGCONFIG(TAG, string_buffer);
 }
 void Epson::start(){
 
@@ -38,9 +38,9 @@ void Epson::start(){
 
 // query status of printer. when online returns value 22.
 int Epson::getStatus(){
-  this->write(0x10);    
-  this->write(0x04);  
-  this->write(1);
+  Epson::write(0x10);    
+  Epson::write(0x04);  
+  Epson::write(1);
   int result;
   result = this->read();
   return result;
@@ -55,9 +55,9 @@ int Epson::read(){
 // Print and feed n lines
 // prints the data in the print buffer and feeds n lines
 void Epson::feed(uint8_t n){
-  this->write(0x1B);  
-  this->write(0x64);
-  this->write(n);    
+  Epson::write(0x1B);  
+  Epson::write(0x64);
+  Epson::write(n);    
 }
 
 // Print one line
@@ -69,16 +69,16 @@ void Epson::feed(){
 // Set line spacing
 // sets the line spacing to n/180-inch
 void Epson::lineSpacing(uint8_t n){
-  this->write(0x1B);  
-  this->write(0x33);
-  this->write(n);  
+  Epson::write(0x1B);  
+  Epson::write(0x33);
+  Epson::write(n);  
 }
 
 // Select default line spacing
 // sets the line spacing to 1/6 inch (n=60). This is equivalent to 30 dots.
 void Epson::defaultLineSpacing(){
-  this->write(0x1B);  
-  this->write(0x32);
+  Epson::write(0x1B);  
+  Epson::write(0x32);
 }
 
 // Select an international character set
@@ -95,110 +95,110 @@ void Epson::defaultLineSpacing(){
 // 10 = Denmark II 
 // see reference for Details! 
 void Epson::characterSet(uint8_t n){
-  this->write(0x1B);  
-  this->write(0x52);
-  this->write(n);  
+  Epson::write(0x1B);  
+  Epson::write(0x52);
+  Epson::write(n);  
 }
 
 
 void Epson::doubleHeightOn(){
-  this->write(0x1B);    
-  this->write(0x21);  
-  this->write(16);
+  Epson::write(0x1B);    
+  Epson::write(0x21);  
+  Epson::write(16);
 }
 
 void Epson::doubleHeightOff(){
-  this->write(0x1B);  
-  this->write(0x21);    
-  this->write(0);
+  Epson::write(0x1B);  
+  Epson::write(0x21);    
+  Epson::write(0);
 }
 
 void Epson::boldOn(){
-  this->write(0x1B);  
-  this->write(0x21);    
-  this->write(8);
+  Epson::write(0x1B);  
+  Epson::write(0x21);    
+  Epson::write(8);
 }
 
 void Epson::boldOff(){
-  this->write(0x1B);  
-  this->write(0x21);    
-  this->write(0);
+  Epson::write(0x1B);  
+  Epson::write(0x21);    
+  Epson::write(0);
 }
 
 void Epson::underlineOff() {
-  this->write(0x1B);  
-  this->write(0x21);    
-  this->write(0);
+  Epson::write(0x1B);  
+  Epson::write(0x21);    
+  Epson::write(0);
 }
 void Epson::underlineOn() {
-  this->write(0x1B);  
-  this->write(0x21);    
-  this->write(128);
+  Epson::write(0x1B);  
+  Epson::write(0x21);    
+  Epson::write(128);
 }
 
 
 // Turn white/black reverse printing mode on/off
 void Epson::reverseOn() {
-  this->write(0x1D);  
-  this->write(0x42);    
-  this->write(1);
+  Epson::write(0x1D);  
+  Epson::write(0x42);    
+  Epson::write(1);
 }
   
 void Epson::reverseOff() {
-  this->write(0x1D);  
-  this->write(0x42);    
-  this->write(0);
+  Epson::write(0x1D);  
+  Epson::write(0x42);    
+  Epson::write(0);
 }
 
 void Epson::justifyLeft() {
-  this->write(0x1B);  
-  this->write(0x61);    
-  this->write(0);
+  Epson::write(0x1B);  
+  Epson::write(0x61);    
+  Epson::write(0);
 }
 
 void Epson::justifyCenter() {
-  this->write(0x1B);  
-  this->write(0x61);    
-  this->write(1);
+  Epson::write(0x1B);  
+  Epson::write(0x61);    
+  Epson::write(1);
 }
 
 void Epson::justifyRight() {
-  this->write(0x1B);  
-  this->write(0x61);    
-  this->write(2);
+  Epson::write(0x1B);  
+  Epson::write(0x61);    
+  Epson::write(2);
 }
 //n range 1-255
 void Epson::barcodeHeight(uint8_t n) {
-  this->write(0x1D);  
-  this->write(0x68);    
-  this->write(n);
+  Epson::write(0x1D);  
+  Epson::write(0x68);    
+  Epson::write(n);
 }
 //n range 2-6
 void Epson::barcodeWidth(uint8_t n) {
-  this->write(0x1D);  
-  this->write(0x77);    
-  this->write(n);
+  Epson::write(0x1D);  
+  Epson::write(0x77);    
+  Epson::write(n);
 }
 //n range 0-3
 void Epson::barcodeNumberPosition(uint8_t n) {
-  this->write(0x1D);  
-  this->write(0x48);    
-  this->write(n);
+  Epson::write(0x1D);  
+  Epson::write(0x48);    
+  Epson::write(n);
 }
 //m range 65-73 (code type)
 //n (digit length)
 void Epson::printBarcode(uint8_t m, uint8_t n) {
-  this->write(0x1D);  
-  this->write(0x6B);    
-  this->write(m);
-  this->write(n);
+  Epson::write(0x1D);  
+  Epson::write(0x6B);    
+  Epson::write(m);
+  Epson::write(n);
 }
 
 void Epson::cut() {
-  this->write(0x1D);
-  this->write('V');
-  this->write(66);
-  this->write(0xA); // print buffer and line feed
+  Epson::write(0x1D);
+  Epson::write('V');
+  Epson::write(66);
+  Epson::write(0xA); // print buffer and line feed
 }
 
 size_t Epson::write(uint8_t c) {
@@ -212,9 +212,9 @@ void Epson::printString(const char* text)
     // Traverse the string 
   for (int i = 0;i<10; i++) { 
     // Print current character 
-      this->write(text[i]); 
+      Epson::write(text[i]); 
   } 
-  this->write(LF);
+  Epson::write(LF);
 }
 
 void Epson::logWrapback(const char* text)
