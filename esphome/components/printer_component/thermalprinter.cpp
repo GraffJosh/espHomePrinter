2b486820684e575a4bb7eaea5292df2c9e5cfd0c
Josh Graff <github@joshuagraff.com>
2023-12-11 17:25:46 -0500

    update namespace

--- esphome/components/printer_component/thermalprinter.cpp
+++ esphome/components/printer_component/thermalprinter.cpp
@@ -320,7 +320,7 @@ void Epson::startTCPServer()
 static void initTCP()
 {
   AsyncServer* server = new AsyncServer(8888); // start listening on tcp port 7050
-	server->onClient(&handleNewClient, server);
+	server->onClient(&Epson::handleNewClient, server);
 	server->begin();
 }
 /* server events */
@@ -331,10 +331,10 @@ static void handleNewClient(void* arg, AsyncClient* client) {
 	clients.push_back(client);
 	
 	// register events
-	client->onData(&handleData, NULL);
-	client->onError(&handleError, NULL);
-	client->onDisconnect(&handleDisconnect, NULL);
-	client->onTimeout(&handleTimeOut, NULL);
+	client->onData(&Epson::handleData, NULL);
+	client->onError(&Epson::handleError, NULL);
+	client->onDisconnect(&Epson::handleDisconnect, NULL);
+	client->onTimeout(&Epson::handleTimeOut, NULL);
 }
 static void handleError(void* arg, AsyncClient* client, int8_t error) {
 	// Epson::printf("\n connection error %s from client %s \n", client->errorToString(error), client->remoteIP().toString().c_str());
