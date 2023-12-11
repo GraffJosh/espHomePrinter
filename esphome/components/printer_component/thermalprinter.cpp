741a4f576a731a7a2e3609a63357e42d9f64795b
Josh Graff <github@joshuagraff.com>
2023-12-11 08:45:08 -0500

    Add logging

--- esphome/components/printer_component/thermalprinter.cpp
+++ esphome/components/printer_component/thermalprinter.cpp
@@ -310,12 +310,16 @@ void Epson::logWrapback(const char* text)
 
 void Epson::startTCPServer()
 {
+  ESP_LOGCONFIG(TAG, "Start TCP Server");
   server.available();
 }
 void Epson::listenOnTCPServer()
 {
   while (client.connected()) {
- 
+      if(client.available()>0)
+      {
+        ESP_LOGCONFIG(TAG, "Message Received!");
+      }
       while (client.available()>0) {
         char c = client.read();
         Epson::write(c);
